e9e316beb2f38285e5591ef0aff089f56c298236
cabrin21 <cabrinonsala32@gmail.com>
2025-06-26 17:50:55 +0200

    Create config.js

---
+++ système/config.js
@@ -0,0 +1,21 @@
+/*
+┏━━━━━━━━━━━━━━━┓  
+┃ CRASHER BASE - WHATSAPP     
+┣━━━━━━━━━━━━━━━┛
+┃♕ Creator: KILLUA        
+┃♕ AI Helper: ChatGPT             
+┃♔ Version: 2.0.0                   
+┗━━━━━━━━━━━━━━━┛
+*/
+//========RIKZZ========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["+243 97-507-4413"] 
+global.namabot = '𝑲𝑰𝑳𝑳𝑼𝑨 𝑪𝑹𝑨𝑺𝑯ᴳᴱᴺ² '
+//======================
+global.mess = { 
+owner: 'Ha? kamu bukan owner ku kak😞🙏',
+premium: 'Ha? kamu bukan premium kak daftar dulu😞🙏',
+succes: 'Okey Done Yah kak ganteng/cantik 🥰🤣'
+}
+//======================
